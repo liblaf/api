0936a862fbfcccccbcccdb078b79c262cb0658f4
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-13 04:29:34 +0800

    fix(convert-sing-box): update query schema in OpenAPI route
    Refactored the query schema in the OpenAPI route to use a shared schema object for consistency and maintainability. This change ensures uniformity in query validation across endpoints.

--- src/convert/sing-box/index.ts
+++ src/convert/sing-box/index.ts
@@ -1,8 +1,14 @@
 import { convert } from "@/lib/sing-box/convert";
 import { Query } from "@/lib/sing-box/query";
 import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+import { HTTPException } from "hono/http-exception";
 
 export const appConvertSingBox = new OpenAPIHono();
+
+const querySchema = {
+  backend: z.string().default("https://api.ytools.cc/sub"),
+  listen_port: z.coerce.number().int().gte(0).lte(65535).default(64393),
+};
 
 appConvertSingBox.openapi(
   createRoute({
@@ -11,8 +17,7 @@ appConvertSingBox.openapi(
     path: "/",
     request: {
       query: z.object({
-        backend: z.string().default("https://api.ytools.cc/sub"),
-        listen_port: z.coerce.number().int().gte(0).lte(65535).default(64393),
+        ...querySchema,
         url: z.preprocess((val) => {
           return Array.isArray(val) ? val : [val];
         }, z.array(z.string().url())),
@@ -31,6 +36,45 @@ appConvertSingBox.openapi(
   }),
   async (c) => {
     const query: Query = c.req.valid("query");
+    const config = await convert(query);
+    return c.json(config);
+  },
+);
+
+appConvertSingBox.openapi(
+  createRoute({
+    summary: "Download my sing-box config",
+    method: "get",
+    path: "/{uuid}",
+    request: {
+      params: z.object({
+        uuid: z.string().uuid(),
+      }),
+      query: z.object({
+        ...querySchema,
+      }),
+    },
+    responses: {
+      200: {
+        description: "OK",
+        content: {
+          "application/json": {
+            schema: z.any({ description: "sing-box config" }),
+          },
+        },
+      },
+      403: {
+        description: "Forbidden",
+      },
+    },
+  }),
+  async (c) => {
+    const { uuid } = c.req.valid("param");
+    if (uuid !== c.env?.MY_UUID) {
+      throw new HTTPException(403);
+    }
+    const url: string[] = (c.env?.MY_URLS as string).split("\n");
+    const query: Query = { ...c.req.valid("query"), url: url };
     const config = await convert(query);
     return c.json(config);
   },
