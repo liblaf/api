3bafd777c9a2ce8608bd8bcd5e3a8e1f638d8420
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-25 14:23:17 +0800

    fix(query.ts): update default value for ipv6 to false
    Changed the default value for ipv6 from true to false to resolve a bug where the incorrect default value was causing unexpected behavior.

--- src/lib/sub/sing-box/query.ts
+++ src/lib/sub/sing-box/query.ts
@@ -5,7 +5,7 @@ import { coerceBoolean } from "@/lib/zod";
 
 export const QuerySchema = z.object({
   backend: z.string().url().default(BACKEND_URL.toString()),
-  ipv6: coerceBoolean().default(true),
+  ipv6: coerceBoolean().default(false),
   listen_port: z.coerce.number().int().min(0).max(65535).default(64393),
   mixed: coerceBoolean().default(true),
   tun: coerceBoolean().default(false),
