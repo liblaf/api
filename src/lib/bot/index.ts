3475c8a22fd17f166b4aa4bc50df35c01357e319
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-28 00:45:08 +0800

    fix: update bot command to use async/await for replying with chat ID

--- src/lib/bot/index.ts
+++ src/lib/bot/index.ts
@@ -2,9 +2,8 @@ import { Bot } from "grammy";
 
 export function newBot(token: string) {
   const bot = new Bot(token);
-  bot.command("getChatId", (ctx) => {
-    console.log(ctx);
-    ctx.reply(ctx.chat.id.toString());
+  bot.command("getChatId", async (ctx) => {
+    await ctx.reply(ctx.chat.id.toString());
   });
   return bot;
 }
