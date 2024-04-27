3d4e53dcd48d20e6c2bab65c9205d1013726bb08
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-28 01:53:47 +0800

    fix: improve message formatting in newBot function
    Refactor message formatting in the newBot function to display usage and expiration information more clearly for users.

--- src/lib/bot/index.ts
+++ src/lib/bot/index.ts
@@ -21,7 +21,39 @@ export function newBot(env: Env) {
       (url: string) => new URL(url),
     );
     const info: UserInfo[] = await fetchInfo(urls);
-    await ctx.reply(JSON.stringify(info));
+    let message = info
+      .map((i) => {
+        let message = `[*${i.name}*](${i.url}):`;
+        if (i.download && i.upload && i.total) {
+          const usage = i.download + i.upload;
+          const ratio = usage / i.total;
+          const emoji = ratio < 0.6 ? "🟢" : ratio < 0.8 ? "🟡" : "🔴";
+          message += ` ${emoji} ${prettyBytes(usage)} / ${prettyBytes(
+            i.total,
+          )}`;
+        }
+        if (i.expire) {
+          const expire = new Date(i.expire * 1000);
+          const remain = Date.now() - i.expire * 1000;
+          const days = Math.floor(remain / 1000 / 86400);
+          const emoji = days < 7 ? "🔴" : days < 14 ? "🟡" : "🟢";
+          message += ` expire at ${prettyDate(expire)} ${emoji}`;
+        }
+      })
+      .join("\n");
+    await ctx.reply(message, { parse_mode: "MarkdownV2" });
   });
   return bot;
+}
+
+function prettyBytes(bytes: number): string {
+  return bytes.toLocaleString(undefined, {
+    style: "unit",
+    unit: "byte",
+    maximumSignificantDigits: 3,
+  });
+}
+
+function prettyDate(date: Date): string {
+  return date.toISOString().slice(0, 10);
 }
