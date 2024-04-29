8ade81075bca60e0e639b65a2846bcf7365bceef
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-29 19:12:35 +0800

    fix: reorder DNS record processing for correct message generation
    Reordered the processing of DNS records to ensure the correct order of messages sent to the chat. This change improves the readability and clarity of the messages sent to users.

--- src/route/bot/send/index.ts
+++ src/route/bot/send/index.ts
@@ -1,7 +1,6 @@
 import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
 
 import { newBot } from "@/lib/bot";
-import { appBotSendDns } from "./dns";
 
 export const appBotSend = new OpenAPIHono();
 
@@ -43,4 +42,54 @@ appBotSend.openapi(
   },
 );
 
-appBotSend.route("/dns", appBotSendDns);
+const DnsRecordSchema = z.object({
+  name: z.string().openapi({ example: "example.com" }),
+  content: z.string().openapi({ example: "8.8.8.8" }),
+});
+
+appBotSend.openapi(
+  createRoute({
+    tags: ["Bot"],
+    summary: "Send DNS update message to chat",
+    method: "post",
+    path: "/{chat_id}/dns",
+    request: {
+      params: z.object({
+        chat_id: z.string().openapi({ example: "1111111111" }),
+      }),
+      body: {
+        content: {
+          "application/json": {
+            schema: z.object({
+              create: z.array(DnsRecordSchema).default([]),
+              delete: z.array(DnsRecordSchema).default([]),
+              keep: z.array(DnsRecordSchema).default([]),
+            }),
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "OK",
+      },
+    },
+  }),
+  async (c) => {
+    const { chat_id } = c.req.valid("param");
+    const { create, delete: del, keep } = c.req.valid("json");
+    const bot = newBot(c.env as any);
+    let message: string = "<b>🔵 Keep</b> <b>🔴 Delete</b> <b>🟢 Create</b>\n";
+    for (const record of keep)
+      message += `🔵 <code>${record.name}</code> => <code>${record.content}</code>\n`;
+    for (const record of del)
+      message += `🔴 <code>${record.name}</code> => <code>${record.content}</code>\n`;
+    for (const record of create)
+      message += `🟢 <code>${record.name}</code> => <code>${record.content}</code>\n`;
+    message = message.trim();
+    const response = await bot.api.sendMessage(chat_id, message, {
+      parse_mode: "HTML",
+    });
+    return c.json(response);
+  },
+);
